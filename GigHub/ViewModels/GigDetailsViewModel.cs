8a74c267e4aa97330b24cf21414101d076b55ac4
Nora <Nora@NORA-PC>
2017-02-08 20:59:15 +0200

    Add project files.

---
+++ GigHub/ViewModels/GigDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class GigDetailsViewModel
+    {
+        public Gig Gig { get; set; }
+
+        public bool IsFollowing { get; set; }
+
+        public bool IsAttending { get; set; }
+    }
+}
