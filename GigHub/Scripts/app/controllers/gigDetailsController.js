28cbbcc89715d627dae37cfe2d87d351ac0028e9
Nora <Nora@NORA-PC>
2017-02-11 15:13:27 +0200

    Modularize JS

---
+++ GigHub/Scripts/app/controllers/gigDetailsController.js
@@ -0,0 +1,42 @@
+﻿// I created a new controller called GigDetailsController.
+//So, this is a new module and it has a dependency to another module called followingService,
+//which is responsible for calling the API. 
+var GigDetailsController = function (followingService) {
+    var followButton;
+
+    // In the inIt method we subscribe to the click event of buttons with the class js, toggle, follow.
+    var init = function () {
+        $(".js-toggle-follow").click(toggleFollowing);
+    };
+
+    //Here's our click handler, toggleFollowing
+    var toggleFollowing = function (e) {
+        //first we get a reference to the button that was clicked,
+        followButton = $(e.target);
+        //from there, we get the followeeId which is a data attribute on the button,
+        var followeeId = followButton.attr("data-user-id");
+
+        //and then we check if the button has the class btn-default. 
+        if (followButton.hasClass("btn-default"))
+            //We ask followingService to create a following object; 
+            followingService.createFollowing(followeeId, done, fail);
+        else
+            //otherwise, we delete the following.
+            followingService.deleteFollowing(followeeId, done, fail);
+    };
+
+    var done = function () {
+        var text = (followButton.text() == "Follow") ? "Following" : "Follow";
+        //In the done method we toggle the btn-info and the btn-default classes and change the text.
+        followButton.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    var fail = function () {
+        // And in the fail method, for now we just display a simple alert.
+        alert("Something failed!");
+    };
+
+    return {
+        init: init
+    }
+}(FollowingService);
---
+++ GigHub/Scripts/app/services/followingService.js
@@ -0,0 +1,24 @@
+﻿// So, this service is another module that has two methods: createFollowing and deleteFollowing.
+//In each of these methods I used jQuery ajax to call our API end point.
+var FollowingService = function () {
+    var createFollowing = function (followeeId, done, fail) {
+        $.post("/api/followings", { followeeId: followeeId })
+        .done(done)
+        .fail(fail);
+
+    };
+
+    var deleteFollowing = function (followeeId, done, fail) {
+        $.ajax({
+            url: "/api/followings" + followeeId,
+            mehod: "DELETE"
+        })
+        .done(done)
+        .fail(fail);
+    };
+
+    return {
+        createFollowing: createFollowing,
+        deleteFollowing: deleteFollowing
+    }
+}();
